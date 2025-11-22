c9f135d20427e2ab46ce998cc09c2ee060784b87
Козин-Поздняков <vasiliy_pit@mail.ru>
2025-11-22 21:38:37 +0300

    geploy настройка

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-//  pages: {
-//     index: {
-//       entry: 'src/main.js',
-//       template: 'public/index.html',
-//       filename: 'index.html'
-//     }
-//   }
+  base: '/modal-get-online-vet-deploy/',
 })
