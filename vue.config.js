874d38c99c69f14418f2bd677beff584884e9d24
Козин-Поздняков <vasiliy_pit@mail.ru>
2025-11-22 21:42:10 +0300

    настройка деплой2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/modal-get-online-vet-deploy/' : '/',
   transpileDependencies: true,
-  base: '/modal-get-online-vet-deploy/',
 })
